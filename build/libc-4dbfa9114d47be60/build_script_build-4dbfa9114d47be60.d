715d2f4648cc79d0491f512daf3550ed3c5305ad
V Venkata Sai Praveen <52025218+vvspraveen@users.noreply.github.com>
2023-09-14 00:33:26 +0530

    Add files via upload

---
+++ build/libc-4dbfa9114d47be60/build_script_build-4dbfa9114d47be60.d
@@ -0,0 +1,5 @@
+/home/ubuntu-new/Desktop/rust/new_server/target/debug/build/libc-4dbfa9114d47be60/build_script_build-4dbfa9114d47be60: /home/ubuntu-new/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.147/build.rs
+
+/home/ubuntu-new/Desktop/rust/new_server/target/debug/build/libc-4dbfa9114d47be60/build_script_build-4dbfa9114d47be60.d: /home/ubuntu-new/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.147/build.rs
+
+/home/ubuntu-new/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.147/build.rs:
